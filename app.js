2a3dde1d22a371796c2e357a532f5262fefebc24
pally1995 <51261320+pally1995@users.noreply.github.com>
2022-08-25 20:21:19 +0100

    ongoing JS functionaility

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let billAmount = document.querySelector("#amountInput");
-const percent = document.getElementsByClassName("percent");
+const percent = Array.from(document.getElementsByClassName("percent"));
 const customPerc = document.querySelector("#customPercent").value;
 const numberOfPeopleValue = document.querySelector(".numberOfPeopleValue");
 const calc = document.querySelector(".calc");
@@ -8,24 +8,38 @@ const tipPerPerson = document.querySelector(".totalPerPerson");
 const totalPerPerson = document.querySelector(".tipPerPerson");
 const reset = document.querySelector(".reset");
 
-calc.addEventListener('click', () => {
-    Array.from(percent).forEach((el) =>
-    el.addEventListener(
-      "click",
-      function () {
-        let buttonText = el.innerHTML;
-        let trimmedText = buttonText.replace("%", "");
-        let percentageSum = (billAmount.value / 100) * trimmedText;
-        let personDivide = percentageSum / numberOfPeopleValue.value;
-        tipPerPerson.innerHTML = percentageSum;
-        totalPerPerson.innerHTML = personDivide;
-        console.log(personDivide);
-      },
-      false
-    )
-  );
-  
-  
-})
+let tipVal = 0;
+let billVal;
+let numberOfPeopleVal;
 
+billAmount.addEventListener("input", () => {
+  billVal = billAmount.value;
+  if (numberOfPeopleVal != 0) {
+    updateTotal();
+    updateTipAmount();
+  }
+});
 
+numberOfPeopleValue.addEventListener("click", () => {
+  numberOfPeopleVal = numberOfPeopleValue.value;
+  if (numberOfPeopleVal != 0) {
+    updateTotal();
+    updateTipAmount();
+  }
+});
+percent.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (billVal != null && numberOfPeopleVal != null) {
+      tipVal = button.innerHTML.replace("%", "");
+      updateTipAmount();
+    }
+  });
+});
+
+function updateTotal() {
+  totalPerPerson.innerHTML = (billVal / 100) * tipVal;
+}
+
+function updateTipAmount() {
+  tipPerPerson.innerHTML = totalPerPerson / numberOfPeopleVal;
+}
