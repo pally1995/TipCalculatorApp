9b03383aebd501d51a1bc18818d135dc471b6f5a
pally1995 <51261320+pally1995@users.noreply.github.com>
2022-08-29 07:50:19 +0100

    js complete

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
 let billAmount = document.querySelector("#amountInput");
 const percent = Array.from(document.getElementsByClassName("percent"));
-const customPerc = document.querySelector("#customPercent").value;
+const customPerc = document.querySelector("#customPercent");
 const numberOfPeopleValue = document.querySelector(".numberOfPeopleValue");
-
 
 const tipPerPerson = document.querySelector(".tipPerPerson");
 const totalTip = document.querySelector(".totalTip");
 const reset = document.querySelector(".reset");
 
-let tipVal;
+let tipVal = 25;
 let billVal;
-let numberOfPeopleVal;
+let numberOfPeopleVal = 1;
 
-tipPerPerson.innerHTML = "$00.00"
-totalTip.innerHTML = "$00.00"
-
-billAmount.addEventListener("input", () => {
-  billVal = billAmount.value;
-  if (billAmount.value <= 0) {
-    tipPerPerson.innerHTML = "$00.00"
-    totalTip.innerHTML = "$00.00"
-  } else {
-    updateTotalTip();
-    updateTipPerPerson();
-  }
-});
-
-numberOfPeopleValue.addEventListener("input", () => {
-  numberOfPeopleVal = numberOfPeopleValue.value;
-  if (numberOfPeopleVal.value <= 0) {
-    tipPerPerson.innerHTML = "$00.00"
-    totalTip.innerHTML = "$00.00"
-  }else {
-    updateTotalTip();
-    updateTipPerPerson();
-  }
-});
+tipPerPerson.innerHTML = "$00.00";
+totalTip.innerHTML = "$00.00";
 
 percent.forEach((button) => {
   button.addEventListener("click", () => {
@@ -47,9 +24,59 @@ percent.forEach((button) => {
   });
 });
 
+percent.forEach((btn) => {
+  btn.addEventListener("click", handleClick);
+});
+
+function handleClick(event) {
+  percent.forEach((btn) => {
+    btn.classList.remove("active");
+    if (event.target.innerHTML === btn.innerHTML) {
+      btn.classList.add("active");
+    }
+  });
+}
+
+billAmount.addEventListener("input", () => {
+  billVal = billAmount.value;
+  updateTipPerPerson();
+  updateTotalTip();
+});
+
+numberOfPeopleValue.addEventListener("input", () => {
+  if (numberOfPeopleValue.value <= 1) {
+    numberOfPeopleVal = 1;
+  } else {
+    numberOfPeopleVal = numberOfPeopleValue.value;
+  }
+  updateTipPerPerson();
+  updateTotalTip();
+});
+
+customPerc.addEventListener("input", () => {
+  tipVal = customPerc.value;
+  percent.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  if (customPerc.value !== 0) {
+    updateTipPerPerson();
+    updateTotalTip();
+  }
+  console.log(tipVal);
+});
+
 function updateTipPerPerson() {
-  tipPerPerson.innerHTML = "$" + ((billVal / 100) * tipVal) / numberOfPeopleVal.toFixed;
+  let tipEach = ((billVal / 100) * tipVal) / numberOfPeopleVal; 
+  tipPerPerson.innerHTML = "$" + tipEach.toFixed(2)
+  
+}
 
 function updateTotalTip() {
-  totalTip.innerHTML =  "$" + (billVal / 100) * tipVal;
+  let fullTip = (billVal / 100) * tipVal;
+  totalTip.innerHTML = "$" + fullTip.toFixed(2);
+  
 }
+
+reset.addEventListener('click', () => {
+  location.reload(true)
+})
